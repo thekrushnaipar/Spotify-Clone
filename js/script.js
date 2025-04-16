1c00e34e77a4b3290cf21c65db2668202f52c642
Krushna Anil Ipar <krushnaipar5551@gmail.com>
2025-04-16 21:06:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
