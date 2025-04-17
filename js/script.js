8661698fb588c577ba73e2fa44aa5b843b612a2b
Krushna Anil Ipar <krushnaipar5551@gmail.com>
2025-04-17 15:38:16 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`https://thekrushnaipar.github.io/Spotify-Clone/songs`)
+    let a = await fetch("https://thekrushnaipar.github.io/Spotify-Clone/songs")
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -74,7 +74,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`https://thekrushnaipar.github.io/Spotify-Clone/songs/`)
+    let a = await fetch("https://thekrushnaipar.github.io/Spotify-Clone/songs/")
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -86,7 +86,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
-            let a = await fetch(`https://thekrushnaipar.github.io/Spotify-Clone/songs/${folder}/info.json`)
+            let a = await fetch("https://thekrushnaipar.github.io/Spotify-Clone/songs/${folder}/info.json")
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
